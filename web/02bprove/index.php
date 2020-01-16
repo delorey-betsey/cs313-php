86152cabec5ff3f4674767e626b115d467ed248b
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-16 12:47:39 -0500

    m

--- web/02bprove/index.php
+++ web/02bprove/index.php
@@ -31,35 +31,16 @@
         <div class="info">
             <div class="description">
                 
-        <!-- countdown counter below-->
-            <div class="redfont" align="right" style="margin: 15px 0px 0px;">
-                
-                <!-- <div align="left" style="width: 140px; border: 1px solid rgb(204, 204, 204); text-align: right; color: rgb(0, 0, 0); font-weight: bold; font-size: 12px; background-color: rgb(255, 255, 255);"> -->
-                <div    
+            <!-- countdown counter below-->
+                <div class="redfont" align="left" style="margin: 15px 0px 0px;font-weight: bold; font-size: 12px;">
+                    <p>My passion is American history.</p>
+                </div>
+                <div align="right" style="width: 140px; border: 1px solid rgb(204, 204, 204); color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">
                     <img src="00homepage/wethepeoplecolor.jpg" alt= "preamble"  style="border:5px solid #e0162b;border-radius:5px" />
                 </div>
-              
-                <!-- <script type="text/javascript" src="http://mycountdown.org/countdown.php?cp2_Hex=ffffff&cp1_Hex=000000&img=1&hbg=&fwdt=250&lab=1&ocd=Race&text1=&text2=&group=My Countdown&countdown=Race&widget_number=3011&event_time=1432339200&timezone=America/Vancouver">
-                </script> -->
-                My passion is American history.
+
             </div>
-        <!-- countdown counter above
-
-                <div align="left" style="width: 140px; border: 1px solid rgb(204, 204, 204); text-align: right; color: rgb(0, 0, 0); font-weight: bold; font-size: 12px; background-color: rgb(255, 255, 255);">
-                    <img src="00homepage/wethepeoplecolor.jpg" alt= "preamble"  style="border:5px solid #e0162b;border-radius:5px" />
-                </div>
-
-                <h2 id="blackfont">Saturday, May 23, 2015</h2>
-
-                <p>Check back often for exciting new work!</p>
-
-            <!-- </div> -->
-
-            <!-- <div>
-                <img src="00homepage/wethepeoplecolor.jpg" alt= "preamble"  style="border:5px solid #e0162b;border-radius:5px" />           
-                <!-- <img src="00homepage/alcatraz1sq365.jpg" alt= "alcatraz island" style="border:5px solid #e0162b;border-radius:5px" /> -->
-
-            </div> 
+ 
         </div> 
 
     </div></main>
