e0473a76828cd7aa6a9778a74c38954db233db9d
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-15 20:02:14 -0500

    m

--- web/scripts.js
+++ web/scripts.js
@@ -3,7 +3,29 @@ function clickMe() {
 	alert("Clicked!");
 }
 
-function changeColor() {
+function div1chgColor() {
+	var textbox_input = "userColor";
+	var newColor = document.getElementById(textbox_input);
+
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = newColor.value;
+	div.style.backgroundColor = color;
+}
+function div2chgColor() {
+	var textbox_input = "userColor";
+	var newColor = document.getElementById(textbox_input);
+
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = newColor.value;
+	div.style.backgroundColor = color;
+}
+function div3chgColor() {
 	var textbox_input = "userColor";
 	var newColor = document.getElementById(textbox_input);
 
