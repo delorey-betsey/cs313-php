19918d7e351e514b5f017d20fec3192ddc8f6d84
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-16 13:02:23 -0500

    m

---
+++ web/02bprove/04siteplan.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Site Plan</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">       
+        <link rel="stylesheet" href="/triathlon/stylesheet.css" media="screen">       
+<!-- Latest compiled and minified JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>  
+    </head>
+
+<!-- COMMENT:   begin webpage  -->
+<body>
+<div class="container">
+    
+<!-- COMMENT:  header     -->
+<?php include 'modules/responsive-header.php'; ?>
+
+<!-- COMMENT:  nav     -->
+<?php include 'modules/responsive-nav.php'; ?>
+
+<div class="row">
+ 
+<main><div>
+<!-- COMMENT:  content     --> 
+<div class="info";
+    <div class="siteplan">
+
+    <h2 class="reversebannertagline">Site Title</h2>
+        <ul>
+            <li>Going Gangbusters Triathlon</li>
+        </ul>
+    <h2 class="reversebannertagline">Site Purpose</h2>
+    <ul>
+        <li>To advertise the Gangbusters Triathlon in San Francisco CA May 23, 2015</li>
+        <li>To provide a form to contact organizers</li>
+        <li>To take online registrations</li>
+    </ul>
+    <h2 class="reversebannertagline">Audience Description</h2>
+    <h4>2013 USA Triathlon Membership Report</h4>
+        <p>Growth in participation in triathlons can be attributed to: society’s interest in fitness and living a healthy lifestyle, the growth of the number of total races across the country, making races easier to get to, the growth in the number of the more accessible shorter sprint races, which made the sport more accessible to those with fewer hours to train each week, media attention on the sport, hrowth in the 30-49 age groups who are looking for varied outlets for fitness, peer pressure from friends who have tried the sport, the ego reward of saying you “are a triathlete”, increase in clubs, which create a community concept for men and especially women who enjoy the group training and support atmosphere, increase in resources (websites, books, magazines) that provide assistance/education in getting started, growth in multisport shops and triathlon specific training and racing gear, marketing and communications efforts of USA Triathlon, growth in the number of USA Triathlon certified coaches who are able to provide training plans and individual attention for athletes who need guidance and motivation.</p>
+        <p>On the event side, the number of races sanctioned through USA Triathlon continues to climb. Those numbers (which include camps and clinics) have nearly tripled over the last seven years, going from 1,541 in 2004 to 4,327 in 2013.  Looking at the distances of the races, the biggest growth continues to be at the shorter sprint distances, which have surged from 818 in 2004 to 1,507 in 2010, but growth at other distances (including Olympic, Ironman 70.3 and Ironman) has also been consistent during that time.  Additionally, a number of events are not sanctioned by USA Triathlon are put on across the United States each year. The majority of these are offered through organizations with insurance and liability programs already in place, such as universities and YMCAs.</p>
+        <p>The sport has consistently grown and continues to thrive in a number of areas. People are choosing to be more active, which leads more people to join in the sport as annual members of USA Triathlon. But the better news is that people are sticking around, doing more events and becoming lifelong members of the multisport community.</p>
+        <p>Who are Triathletes?  In the late 1990s there was a slow increase in overall membership but a sizeable shift in the participation base from 30-34 to 35-39. Since that time the greatest growth has occurred in the 35-39 and 40-44 age groups. We believe that this growth will continue as those in these age groups are looking for new outlets of participation and fitness. Fortunately growth has been good in all age categories in the past decade and so the sport as a whole should continue to increase as more events are being started in smaller triathlon markets.</p>
+        <p>Female Participation:  Since 2000, USA Triathlon's female membership has grown from 27 percent of the total of the annual members to more than 36.5 percent at the end of 2013. Factors leading to this growth are society’s acceptance of “active” women, women feeling more comfortable living an active lifestyle, the growth of women’s-only events like the Danskin and Trek Triathlon Series, and races focusing on charity involvement and fundraising.</p>
+        <p>Youth Growth:  USA Triathlon categorizes youth athletes as ages 7-17. At present, youth triathlon is still in a very organic, grassroots state. Many — if not most — races are organized by groups that already service young populations. We are seeing an increase in the number of “adult” triathlons that are adding youth events to their program in what is commonly called a “triathlon festival.” The idea is to provide a menu of events over the course of a weekend, from sprint to long course distances, to attract as many people as possible. Race directors have realized a growing demand to provide something for the young kids who may join mom and dad at the triathlon festival. In this way, triathlon is evolving into a multi-generational sporting event … all taking place on the same weekend.</p>
+        <p>Today’s younth also have the advantage of recognizing triathlon as a “real” sport. It is not a fringe sport to them as it was when many adults got their start in the late-80s and early-90s. The sport is televised more often — including broadcasts on NBC, Universal Sports Network and regional sports networks — and the web has made information much more accessible than it was even in 2000 when triathlon debuted in the Olympics.</p>
+        <p>A study initiated by USA Triathlon and conducted by the TribeGroup in early 2009 revealed exciting new data on the demographics and spending habits of multisport athletes in the United States.</p>
+        <p>Key Findings:  The study grouped participants into seven distinct segments according to similarities in their demographics, psychographics and involvement in the sport. On average, triathletes are from high socio-economic backgrounds with median incomes of $126,000, however, their motivators and mindset are different, creating a significant opportunity for the triathlon community to serve them better and in more targeted ways. Unlike many consumers, they are spending cautiously but on average intend to spend as much in 2009 as they did in 2008. In fact, 45 percent would rather spend discretionary income on triathlon with three of seven segments exceeding 65 percent and one nearly 80 percent.</p>
+        <p>General Participation:  Older athletes do more races 95% participate for the personal challenge 87% participate to stay in shape On average, they participated in 4.2 triathlons during the previous 12 months 86% plan to do longer races in the future Sprint triathlon remains the most popular, with those events attracting participation of more than three quarters of respondents</p>
+        <p>What Distances?  78% participate in Sprint (approx. 500 meter swim, 12 mile bike, 5k run) 58% participate in Olympic (1.5k swim, 40k bike, 10k run) 39% participate in Half Ironman (1.2 mile swim, 56 mile bike, 13.1 mile run) 17% participate in Ironman (2.4 mile swim, 112 mile bike, 26.2 mile run) 15% participate in Short duathlon (approx. 5k run, 30k bike, 5k run) 4% participate in Long duathlon (approx. 10k run, 70k bike, 10k run)</p>
+
+    <h2 class="reversebannertagline">Complete Asset List</h2>
+<table>
+    <col style="width:60%">
+    <col style="width:40%">
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Homepage</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Hype</li>
+                <li>Images</li>
+                <li>Dates</li>
+                <li>Contact form</li>
+                <li>Countdown counter</li>
+            </ul>
+        </td>
+    </tr>
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Run Route</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Race info</li>
+                <li>Image</li>
+                <li>Map</li>
+            </ul>
+        </td>
+    </tr>
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Swim Route</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Race info</li>
+                <li>Image</li>
+                <li>Map</li>
+            </ul>
+        </td>
+    </tr>
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Bike Route</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Race info</li>
+                <li>Image</li>
+                <li>Map</li>
+            </ul>
+        </td>
+    </tr>
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Registration</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Images</li>
+                <li>Dates</li>
+                <li>Registration form</li>
+                <li>Countdown counter</li>
+            </ul>
+        </td>
+    </tr>
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Contact Us</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Images</li>
+                <li>Contact form</li>
+            </ul>
+        </td>
+    </tr>
+    <tr class="tabletr">
+        <td>
+            <h3 style="text-align:center">Site Plan</h3>
+        </td>
+        <td>
+            <ul>
+                <li>Site title</li>
+                <li>Site purpose</li>
+                <li>Audience</li>
+                <li>Asset list</li>
+                <li>Style Guide</li>
+            </ul>
+        </td>
+    </tr>     
+</table>
+    
+    <h2 class="reversebannertagline">Complete Style Guide</h2>
+    
+    <h4 class="redfont"><br><br>STYLE GUIDE: COLOR SCHEME</h4>
+    <p>Red, white and blue, black strips, ball & chain logo.</p>
+    
+    <img src="04siteplan/colorscheme.JPG" alt="color scheme" style="width:25%;height:25%;">
+    
+    <h4 class="redfont"><br><br>STYLE GUIDE: TYPOGRAPHY</h4>
+    
+    <p>Titling: Stencil font for government, military, font precision look.</p>
+    <img src="04siteplan/stencil.jpg" alt="color scheme" style="width:25%;height:25%;">
+    <p><br>Sub-headings: Klint Std font for a very clean, neat, modern font.  Easy to read at a glance for those in a hurry yet a little quirky/funky for those who are “out there.”</p>
+    <img src="04siteplan/Klint.JPG" alt="color scheme" style="width:25%;height:25%;">
+    <p><br>Text Content: Arial font for readability with no distractions.</p>
+    <img src="04siteplan/arial.png" alt="color scheme" style="width:25%;height:25%;">
+    
+    <h4 class="redfont"><br><br>STYLE GUIDE: NAVIGATION</h4>
+    
+    <p><br>Site navigation will be via a horizontal top menu bar with drop down menus.  A single means of navigation that will be consistent on all pages.</p>
+    <img src="04siteplan/navigation.JPG" alt="color scheme" style="width:50%;height:50%;">
+    
+    <h4 class="redfont"><br><br>STYLE GUIDE: RESPONSIVENESS</h4>
+    <table>
+        <col style="width:30%">
+        <col style="width:70%">
+        <tr class="tabletr">
+            <td><h3><br>Browser:</h3></td>
+            <td><img class="rotate90" src="04siteplan/browser.jpg" alt="browser screen design"  style="width:60%">
+            </td>
+        </tr>
+        <tr class="tabletr">
+            <td><h3><br>Tablet:</h3></td>
+            <td><img class="rotate90" src="04siteplan/tablet.jpg" alt="tablet screen design" style="width:60%">
+            </td>
+        </tr>
+        <tr class="tabletr">
+            <td><h3><br>Smartphone:</h3></td>
+            <td><img class="rotate90" src="04siteplan/smartphone.jpg" alt="smartphone design" style="width:60%">
+            </td>
+        </tr>
+    </table>
+</div>
+</div> 
+</div></main>
+
+<!-- COMMENT:  footer     -->
+<footer>
+ <?php include 'modules/responsive-footer-nav.php'; ?>
+</footer>
+
+</div>
+</div>
+</body>
+</html>
