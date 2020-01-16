3da8e7fa5fbee1d3e5e405f6bd47684517afb0c9
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-16 13:46:24 -0500

    m

--- web/02bprove/modules/underconstruction.php
+++ web/02bprove/modules/underconstruction.php
@@ -1,6 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Run Route</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">       
+        <link rel="stylesheet" href="/02bprove/stylesheet.css" media="screen">       
+<!-- Latest compiled and minified JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>  
+    </head>
+
+<!-- COMMENT:   begin webpage  -->
+<body>
+<div class="container">
+    
+<!-- COMMENT:  banner     -->
+<?php include 'modules/responsive-header.php'; ?>
+
+<!-- COMMENT:  navigation     -->
+<?php include 'modules/responsive-nav.php'; ?>
+
+<div class="row">
+ 
+<main><div>        
+<!-- COMMENT:  content   -->
+ 
 <div>
-    <a href="index.php">    
-    <img src="00homepage/comingsoon.jpg" 
-         alt= "under construction" style="margin-top:10px;height:91%;width:91%;padding-left:10%">
-    </a>       
+    <a href="https://museum.archives.gov/founding-documents">
+        <img src="00homepage/comingsoon.jpg" alt= "under construction"  style="border:5px solid #e0162b;border-radius:5px" />
+    </a>        
 </div>
+
+</div></main>
+</div>
+
+<!-- COMMENT:  footer     -->
+<footer>
+ <?php include 'modules/responsive-footer-nav.php'; ?>
+</footer>
+
+</div>
+</body>
+</html>
