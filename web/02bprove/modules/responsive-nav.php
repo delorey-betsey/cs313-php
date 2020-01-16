170b29a0f798adf850eb29c81e20cb33a6cac18b
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-16 12:20:34 -0500

    m

--- web/02bprove/modules/responsive-nav.php
+++ web/02bprove/modules/responsive-nav.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
+  
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -16,33 +17,33 @@
         <ul class="nav navbar-nav">
            
              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Routes<span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Assignments<span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                    <li><a href="01runroute.php">Run Route</a></li>
-                    <li><a href="01swimroute.php">Swim Route</a></li>
-                    <li><a href="01bikeroute.php">Bike Route</a></li>
+                    <li><a href="01runroute.php">Week 01</a></li>
+                    <li><a href="01swimroute.php">Week 02 Teach</a></li>
+                    <li><a href="01bikeroute.php">Week 03</a></li>
                 </ul>
             </li>
                      
-            <li><a href="02registration.php" >Registration</a></li>
+            <!-- <li><a href="02registration.php" >Assignments</a></li>
             
-            <li><a href="03contact.php" >Contact Us</a></li>
+            <li><a href="03contact.php" >Coming Soon</a></li> -->
                         
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">CIT230<span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Coming Soon<span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
                     <li><a href="04siteplan.php">Site Plan</a></li>
                 </ul>
             </li>
             
-            <ul class="nav navbar-nav navbar-right">
+            <!-- <ul class="nav navbar-nav navbar-right">
                 <li>
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" id="blackfont">FROM SAN QUENTIN TO ALCATRAZ AND BACK AGAIN...<span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" id="blackfont">Land of the Free, Home of the Brave<span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="02registration/registrationformindex.php">Registration Form</a></li>
                     </ul>	
                 </li>          
-            </ul>
+            </ul> -->
             
         </ul> 
         <!-- /.navbar-collapse -->
