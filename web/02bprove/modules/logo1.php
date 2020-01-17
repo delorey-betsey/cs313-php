a4dc598d90d99157c5e7c83cb8d7a7275317df8e
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-17 10:54:13 -0500

    m

--- web/02bprove/modules/logo1.php
+++ web/02bprove/modules/logo1.php
@@ -1,6 +1,7 @@
 <div>
     <a href="index.php">    
     <img src="00homepage/flag.jpg"
-         alt= "american flag and eagle" style="height:91%;width:91%;padding-left:10%">>
+         alt= "american flag and eagle" 
+         style="height:91%;width:91%;padding-left:10%;padding-top:5%;">>
     </a>       
 </div>
