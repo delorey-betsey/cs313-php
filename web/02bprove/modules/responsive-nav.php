9f6359b6a6b383a4874c09ce95aea6bf5fdbdbbb
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-17 10:18:40 -0500

    m

--- web/02bprove/modules/responsive-nav.php
+++ web/02bprove/modules/responsive-nav.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
   
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
@@ -16,30 +15,7 @@
         
         <ul class="nav navbar-nav">
 
-            <!-- Assignments Drop Down-->
-
-             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Assignments<span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="week01.php">Week 01</a></li>
-                    <li><a href="week02teach.php">Week 02 Teach</a></li>
-                    <li><a href="underconstruction.php">Week 02 Prove</a></li>
-                </ul>
-            </li>
-                     
-           <!-- Coming Soon Drop Down-->
-            
-            <li><a href="03contact.php" >Coming Soon</a></li> -->
-                        
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Coming Soon<span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="underconstruction.php">Under Construction</a></li>
-                </ul>
-            </li>
-
             <!-- Betsey Delorey Drop Down-->
-
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">BYU-I CS313 Web Engineering II: Betsey Delorey<span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
@@ -47,6 +23,26 @@
                 </ul>
             </li>
             
+            <!-- Coming Soon Drop Down-->
+            <li><a href="03contact.php" >Coming Soon</a></li> -->
+            
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Coming Soon<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="underconstruction.php">Under Construction</a></li>
+                </ul>
+            </li>
+
+            <!-- Assignments Drop Down-->
+             <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Assignments<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="week01.php">Week 01</a></li>
+                    <li><a href="week02teach.php">Week 02 Teach</a></li>
+                    <li><a href="underconstruction.php">Week 02 Prove</a></li>
+                </ul>
+            </li>                  
+           
         </ul> 
         <!-- /.navbar-collapse -->
     </div>  
