47122db2996a7ce8a8565ed7fe76ca8f539d7f24
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-21 09:48:42 -0500

    m

---
+++ web/03teach/results.php
@@ -0,0 +1,20 @@
+<?php
+$username = $_POST[username];
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Betsey Delorey - 03 Teach RESULTS</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+	<body>
+        <!-- format return results -->
+
+        <h1>Welcome user: <?php echo $username; ?></h1>
+
+        </form>
+	</body>
+</html>
