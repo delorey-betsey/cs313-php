27f08ad8c48b1bbfa03e4601d0baccc7eab07b52
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 15:56:19 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -7,24 +7,28 @@
         <div>
             <img  class="art" src="images/dog1.jpg"  alt= "dog1"  /> 
             <a  href="#" title="dog1" >Item 1</a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>
     <br><br>
 
         <div>    
             <img  class="art" src="images/dog2.jpg"  alt= "dog2"  /> 
             <a href="#" title="dog2" >Item 2</a> 
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>
     <br><br>
 
         <div>    
             <img  class="art" src="images/cat1.jpg"  alt= "cat1"  /> 
             <a  href="#" title="cat1" >Item 3</a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>       
     <br><br>
 
         <div>     
             <img  class="art" src="images/cat3.jpg"  alt= "cat3"  /> 
             <a href="#" title="cat3" >Item 4</a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>
     <br><br>   
 
