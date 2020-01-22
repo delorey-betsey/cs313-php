2b3fc5a1116068c6519c978585da85fd3714bca3
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 15:23:39 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -5,36 +5,36 @@
 <div class="grouping">
     
     <div>
-        <img id="imgbbq" src="images/dog1.jpg"  alt= "dog1"  /> 
+        <img  src="images/dog1.jpg"  alt= "dog1"  /> 
     </div>
-    <a id="bbq" href="#" title="dog1" >Item 1</a>
+    <a  href="#" title="dog1" >Item 1</a>
 </div>
 <br><br>
 
 <div class="grouping">
 
     <div>    
-        <img id="imgpotpie" src="images/dog2.jpg"  alt= "dog2"  /> 
+        <img  src="images/dog2.jpg"  alt= "dog2"  /> 
     </div>
-    <a id="potpie" href="#" title="dog2" >Item 2</a> 
+    <a href="#" title="dog2" >Item 2</a> 
  </div>
  <br><br>
 
 <div class="grouping">
 
     <div>    
-        <img id="imgsoup" src="images/cat1.jpg"  alt= "cat1"  /> 
+        <img  src="images/cat1.jpg"  alt= "cat1"  /> 
     </div>
-    <a id="soup" href="#" title="cat1" >Item 3</a>  
+    <a  href="#" title="cat1" >Item 3</a>  
 </div>
 <br><br>
 
 <div class="grouping">  
 
     <div>     
-        <img id="imgtaco" src="images/cat3.jpg"  alt= "cat3"  /> 
+        <img  src="images/cat3.jpg"  alt= "cat3"  /> 
     </div>
-    <a id="taco" href="#" title="cat3" >Item 4</a>   
+    <a href="#" title="cat3" >Item 4</a>   
  </div>   
  <br><br>   
 </div>
