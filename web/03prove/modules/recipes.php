bb68fa7327f958ee5043767fc526d5cc058c4571
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 15:29:05 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -4,40 +4,31 @@
 <br><br>
 <div class="grouping">
     
-    <div>
-        <img  src="images/dog1.jpg"  alt= "dog1"  /> 
-    </div>
-    <a  href="#" title="dog1" >Item 1</a>
+        <div>
+            <img  src="images/dog1.jpg"  alt= "dog1"  /> 
+            <a  href="#" title="dog1" >Item 1</a>
+        </div>
+    <br><br>
+
+        <div>    
+            <img  src="images/dog2.jpg"  alt= "dog2"  /> 
+            <a href="#" title="dog2" >Item 2</a> 
+        </div>
+    <br><br>
+
+        <div>    
+            <img  src="images/cat1.jpg"  alt= "cat1"  /> 
+            <a  href="#" title="cat1" >Item 3</a>
+        </div>       
+    <br><br>
+
+        <div>     
+            <img  src="images/cat3.jpg"  alt= "cat3"  /> 
+            <a href="#" title="cat3" >Item 4</a>
+        </div>
+    <br><br>   
+
 </div>
-<br><br>
 
-<div class="grouping">
-
-    <div>    
-        <img  src="images/dog2.jpg"  alt= "dog2"  /> 
-    </div>
-    <a href="#" title="dog2" >Item 2</a> 
- </div>
- <br><br>
-
-<div class="grouping">
-
-    <div>    
-        <img  src="images/cat1.jpg"  alt= "cat1"  /> 
-    </div>
-    <a  href="#" title="cat1" >Item 3</a>  
-</div>
-<br><br>
-
-<div class="grouping">  
-
-    <div>     
-        <img  src="images/cat3.jpg"  alt= "cat3"  /> 
-    </div>
-    <a href="#" title="cat3" >Item 4</a>   
- </div>   
- <br><br>   
-</div>
-<div>
 
   
