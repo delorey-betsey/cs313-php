cf1d3461b163dcacef1c944debaf9329d1bd19c7
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 15:19:01 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -4,32 +4,40 @@
 <br><br>
 <div class="grouping">
     
-    <div class="bbq">
+    <div>
         <img id="imgbbq" src="images/dog1.jpg"  alt= "dog1"  /> 
     </div>
     <a id="bbq" href="#" title="dog1" >Item 1</a>
 </div>
 <br><br>
+
 <div class="grouping">
-    <div class="potpie">    
+
+    <div>    
         <img id="imgpotpie" src="images/dog2.jpg"  alt= "dog2"  /> 
     </div>
     <a id="potpie" href="#" title="dog2" >Item 2</a> 
  </div>
  <br><br>
+
 <div class="grouping">
-    <div class="soup">    
+
+    <div>    
         <img id="imgsoup" src="images/cat1.jpg"  alt= "cat1"  /> 
     </div>
     <a id="soup" href="#" title="cat1" >Item 3</a>  
 </div>
 <br><br>
-<div class="grouping">    
-    <div class="taco">     
+
+<div class="grouping">  
+
+    <div>     
         <img id="imgtaco" src="images/cat3.jpg"  alt= "cat3"  /> 
     </div>
     <a id="taco" href="#" title="cat3" >Item 4</a>   
  </div>   
  <br><br>   
 </div>
+<div>
+
   
