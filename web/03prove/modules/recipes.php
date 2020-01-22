514f0be889b3208dd0e30b6fccc7c80352d12414
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 16:09:23 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -4,29 +4,29 @@
 <br><br>
 <div class="grouping">
     
-        <div>
-            <img  class="art" src="images/dog1.jpg"  alt= "dog1"  /> 
+        <div class="art">
+            <img   src="images/dog1.jpg"  alt= "dog1"  /> 
             <a  href="#" title="dog1" >Item 1</a>
             <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>
     <br><br>
 
-        <div>    
-            <img  class="art" src="images/dog2.jpg"  alt= "dog2"  /> 
+    <div class="art">   
+            <img  src="images/dog2.jpg"  alt= "dog2"  /> 
             <a href="#" title="dog2" >Item 2</a> 
             <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>
     <br><br>
 
-        <div>    
-            <img  class="art" src="images/cat1.jpg"  alt= "cat1"  /> 
+    <div class="art">    
+            <img  src="images/cat1.jpg"  alt= "cat1"  /> 
             <a  href="#" title="cat1" >Item 3</a>
             <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>       
     <br><br>
 
-        <div>     
-            <img  class="art" src="images/cat3.jpg"  alt= "cat3"  /> 
+    <div class="art">    
+            <img  src="images/cat3.jpg"  alt= "cat3"  /> 
             <a href="#" title="cat3" >Item 4</a>
             <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
         </div>
