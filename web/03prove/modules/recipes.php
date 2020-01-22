6e14fa20cc9e9fec518e04ff66a0745d0de129fe
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 14:48:06 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -8,27 +8,27 @@
     </div>
     <h3><a id="bbq" href="#" title="dog1" >Item 1</a>
 </div>
-
+<br><br>
 <div class="grouping">
     <div class="potpie">    
         <img id="imgpotpie" src="images/dog2.jpg"  alt= "dog2"  /> 
     </div>
     <h3><a id="potpie" href="#" title="dog2" >Item 2</a> </h3>
  </div>
-
+ <br><br>
 <div class="grouping">
     <div class="soup">    
         <img id="imgsoup" src="images/cat1.jpg"  alt= "cat1"  /> 
     </div>
     <h3><a id="soup" href="#" title="cat1" >Item 3</a> </h3> 
 </div>
-
+<br><br>
 <div class="grouping">    
     <div class="taco">     
         <img id="imgtaco" src="images/cat3.jpg"  alt= "cat3"  /> 
     </div>
     <h3><a id="taco" href="#" title="cat3" >Item 4</a> </h3>  
  </div>   
-   
+ <br><br>   
 </div>
   
--- web/03prove/modules/title.php
+++ web/03prove/modules/title.php
@@ -1,1 +1,3 @@
 Welcome to ARfS!  ART for Sale
+<br>
+All proceeds benefit Dane County Humane Society.
