174fdf62ec766c813b1c3fa351af6580f60d0382
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 16:21:22 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -1,4 +1,4 @@
-<!--      Recipe Text-->
+<!--  inventory   -->
 
 <p>All proceeds benefit Dane County Humane Society.</p>  
 
@@ -7,28 +7,28 @@
         <div class="art">
             <img   src="images/dog1.jpg"  alt= "dog1"  /> 
             <a  href="#" title="dog1" >Item 1</a>
-            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a>
         </div>
     <br><br>
 
     <div class="art">   
             <img  src="images/dog2.jpg"  alt= "dog2"  /> 
             <a href="#" title="dog2" >Item 2</a> 
-            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a>
         </div>
     <br><br>
 
     <div class="art">    
             <img  src="images/cat1.jpg"  alt= "cat1"  /> 
             <a  href="#" title="cat1" >Item 3</a>
-            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a>
         </div>       
     <br><br>
 
     <div class="art">    
             <img  src="images/cat3.jpg"  alt= "cat3"  /> 
             <a href="#" title="cat3" >Item 4</a>
-            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a>
+            <a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a>
         </div>
     <br><br>   
 
