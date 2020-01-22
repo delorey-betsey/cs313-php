865660700070f25a2f4d04309e3aa8728811623a
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 14:00:58 -0500

    m

--- web/03prove/modules/tools.php
+++ web/03prove/modules/tools.php
@@ -1,2 +1,2 @@
-    <img id="tools" src="images/cart.jpg"  alt= "ACME"  /> 
+    <img id="tools" src="images/cart.png"  alt= "ACME"  /> 
     <a id="myaccount" href="#" title="My Account" >My Cart</a>
