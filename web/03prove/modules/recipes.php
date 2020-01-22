728beb05372b2191b0d25f1de329e79ab91521f3
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-22 15:39:58 -0500

    m

--- web/03prove/modules/recipes.php
+++ web/03prove/modules/recipes.php
@@ -5,25 +5,25 @@
 <div class="grouping">
     
         <div>
-            <img  src="images/dog1.jpg"  alt= "dog1"  /> 
+            <img  class="art" src="images/dog1.jpg"  alt= "dog1"  /> 
             <a  href="#" title="dog1" >Item 1</a>
         </div>
     <br><br>
 
         <div>    
-            <img  src="images/dog2.jpg"  alt= "dog2"  /> 
+            <img  class="art" src="images/dog2.jpg"  alt= "dog2"  /> 
             <a href="#" title="dog2" >Item 2</a> 
         </div>
     <br><br>
 
         <div>    
-            <img  src="images/cat1.jpg"  alt= "cat1"  /> 
+            <img  class="art" src="images/cat1.jpg"  alt= "cat1"  /> 
             <a  href="#" title="cat1" >Item 3</a>
         </div>       
     <br><br>
 
         <div>     
-            <img  src="images/cat3.jpg"  alt= "cat3"  /> 
+            <img  class="art" src="images/cat3.jpg"  alt= "cat3"  /> 
             <a href="#" title="cat3" >Item 4</a>
         </div>
     <br><br>   
