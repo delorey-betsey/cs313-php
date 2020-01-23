2627b50112c617af29e634f106fe6932976d38fe
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-23 13:19:47 -0500

    m

--- web/03prove/modules/inventory.php
+++ web/03prove/modules/inventory.php
@@ -12,21 +12,21 @@
     <br><br>
 
     <div class="art">   
-        <div><a class="item" href="#" title="dog2" >Item 2</a> </div>
+        <div><a class="item" href="../03prove/modules/dog2.php" title="dog2" >Item 2</a> </div>
         <div><img  src="images/dog2.jpg"  alt= "dog2"  /> </div>            
         <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
     </div>
     <br><br>
 
     <div class="art">    
-        <div><a class="item"  href="#" title="cat1" >Item 3</a></div>
+        <div><a class="item"  href="../03prove/modules/cat1.php" title="cat1" >Item 3</a></div>
         <div><img  src="images/cat1.jpg"  alt= "cat1"  /> </div>            
         <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
         </div>       
     <br><br>
 
     <div class="art">    
-        <div><a class="item"  href="#" title="cat3" >Item 4</a></div>
+        <div><a class="item"  href="../03prove/modules/cat2.php" title="cat2" >Item 4</a></div>
         <div><img  src="images/cat3.jpg"  alt= "cat3"  /> </div>            
         <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
     </div>
