d4d370ce38989d57d7f8d3a50ac56e5a17c74834
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-23 13:10:39 -0500

    m

--- web/03prove/modules/dog1.php
+++ web/03prove/modules/dog1.php
@@ -3,5 +3,5 @@
         echo ("Hello");
         ?>
         <div>
-            <img src="/images/full/dog1.jpg" alt="myPic" />
+            <img src="../images/full/dog1.jpg" alt="myPic" />
         </div>
