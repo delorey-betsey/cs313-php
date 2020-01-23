e461ef5dd2e4171f15b61fd8b30a5bc956d561e9
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-23 12:29:05 -0500

    m

--- web/03prove/modules/inventory.php
+++ web/03prove/modules/inventory.php
@@ -4,32 +4,32 @@
 
 <div class="grouping">
     
-        <div class="art">
-            <div><img   src="images/dog1.jpg"  alt= "dog1"  /> </div>
-            <div><a  class="item" href="#" title="dog1" >Item 1</a></div>
-            <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
-        </div>
+    <div class="art">
+        <div><a  class="item" href="#" title="dog1" >Item 1</a></div>
+        <div><img   src="images/dog1.jpg"  alt= "dog1"  /> </div>            
+        <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
+    </div>
     <br><br>
 
     <div class="art">   
-            <div><img  src="images/dog2.jpg"  alt= "dog2"  /> </div>
-            <div><a class="item" href="#" title="dog2" >Item 2</a> </div>
-            <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
-        </div>
+        <div><a class="item" href="#" title="dog2" >Item 2</a> </div>
+        <div><img  src="images/dog2.jpg"  alt= "dog2"  /> </div>            
+        <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
+    </div>
     <br><br>
 
     <div class="art">    
-            <div><img  src="images/cat1.jpg"  alt= "cat1"  /> </div>
-            <div><a class="item"  href="#" title="cat1" >Item 3</a></div>
-            <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
+        <div><a class="item"  href="#" title="cat1" >Item 3</a></div>
+        <div><img  src="images/cat1.jpg"  alt= "cat1"  /> </div>            
+        <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
         </div>       
     <br><br>
 
     <div class="art">    
-            <div><img  src="images/cat3.jpg"  alt= "cat3"  /> </div>
-            <div><a class="item"  href="#" title="cat3" >Item 4</a></div>
-            <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
-        </div>
+        <div><a class="item"  href="#" title="cat3" >Item 4</a></div>
+        <div><img  src="images/cat3.jpg"  alt= "cat3"  /> </div>            
+        <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div>
+    </div>
     <br><br>   
 
 </div>
