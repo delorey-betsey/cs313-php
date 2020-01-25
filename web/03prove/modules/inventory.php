1b6b9ef0098d43ca95977a7269d84b0bb190a6e6
Betsey Delorey <byuideloreyb@gmail.com>
2020-01-24 22:59:44 -0500

    m

--- web/03prove/modules/inventory.php
+++ web/03prove/modules/inventory.php
@@ -7,43 +7,39 @@
     <div class="grouping">
 
         <div class="art">
-            <div><a  class="item" href="../03prove/modules/dog1.php" title="dog1" >Item 1</a></div>
-            <div><img   src="images/dog1.jpg"  alt= "dog1"  /> </div>  
-            <!-- <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div> -->       
+            <div><a  class="item" href="../03prove/modules/dog1.php" title="dog1" >Item 1</a></div>       
             <input class=“largerCheckbox” type="checkbox" id="item1" name="items[]" value="item1">
+            <div><img   src="images/dog1.jpg"  alt= "dog1"  /> </div>
             <label for="item1">Item 1 - $10</label>
         </div>
         <br><br>
 
         <div class="art">   
             <div><a class="item" href="../03prove/modules/dog2.php" title="dog2" >Item 2</a> </div>
-            <div><img  src="images/dog2.jpg"  alt= "dog2"  /> </div>  
-            <!-- <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div> -->
             <input class=“largerCheckbox” type="checkbox" id="item2" name="items[]" value="item2">
+            <div><img  src="images/dog2.jpg"  alt= "dog2"  /> </div>
             <label for="item2">Item 2 - $20</label>
         </div>
         <br><br>
 
         <div class="art">    
             <div><a class="item"  href="../03prove/modules/cat1.php" title="cat1" >Item 3</a></div>
-            <div><img  src="images/cat1.jpg"  alt= "cat1"  /> </div>            
-            <!-- <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div> -->
             <input class=“largerCheckbox” type="checkbox" id="item3" name="items[]" value="item3">
+            <div><img  src="images/cat1.jpg"  alt= "cat1"  /> </div>
             <label for="item3">Item 3 - $30</label>
         </div>       
         <br><br>
 
         <div class="art">    
             <div><a class="item"  href="../03prove/modules/cat2.php" title="cat2" >Item 4</a></div>
-            <div><img  src="images/cat3.jpg"  alt= "cat3"  /> </div> 
-            <!-- <div><a href="#"><img id="actionbtn" alt="Add to cart button" src="images/addbutton.jpg"></a></div> -->
+            <div><img  src="images/cat3.jpg"  alt= "cat3"  /> </div>
             <input class=“largerCheckbox” type="checkbox" id="item4" name="items[]" value="item4">
             <label for="item4">Item 4 - $40</label>
         </div>
         <br><br>  
 
         <!-- <input type="submit" >  -->
-        <input type="submit" value="" style="background-image: <img src='images/addbutton.jpg'>;">
+        <button type="submit" name="addbutton" value="addbutton"><img src="images/addbutton.jpg" alt="addbutton"></button>
         
 
     </div>
