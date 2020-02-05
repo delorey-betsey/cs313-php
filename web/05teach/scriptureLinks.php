f2d5f84d3f01aa3a907830efb17490169b03e32b
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-05 09:19:36 -0500

    m

--- web/05teach/scriptureLinks.php
+++ web/05teach/scriptureLinks.php
@@ -2,7 +2,7 @@
 if (!isset($_GET['scripture_id']))
 { die("Error, id not specified...");
 }
-$scripture_id = htmlspecialchars($GET['scripture_id']);
+$scripture_id = htmlspecialchars($_GET['scripture_id']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,6 +13,7 @@ $scripture_id = htmlspecialchars($GET['scripture_id']);
 <body>
 
  <h1>Scripture Content for Scripture ID <?php echo $scripture_id ?></h1>
+<p>hello betsey</p>
 
 
 </body>
