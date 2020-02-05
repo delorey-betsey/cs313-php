9a202df64a22bdcdcf4c7b679b1e23f98d051a64
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-05 13:45:17 -0500

    m

--- web/05teach/scriptureLinks.php
+++ web/05teach/scriptureLinks.php
@@ -1,15 +1,16 @@
 <?php
 $scripture_id = htmlspecialchars($_GET['scripture_id']);
-
 require "dbConnect.php";
 $db = get_db();
-
+SELECT CURRENT_TIMESTAMP;
+echo CURRENT_TIMESTAMP;
 //SELECT book, chapter, verse, content FROM scriptureBD by id;
  
 $stmt = $db->prepare('SELECT book, chapter, verse, content FROM scriptureBD WHERE id = :id');
 $stmt->bindValue(':id', $scripture_id, PDO::PARAM_INT);
 $stmt->execute();
 $scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+VAR_DUMP($scriptures);
 ?>
 
 <!DOCTYPE html>
