0698850f71c94ad072700b798615491e59f93952
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-05 13:56:37 -0500

    m

--- web/05teach/scriptureLinks.php
+++ web/05teach/scriptureLinks.php
@@ -3,7 +3,12 @@ $scripture_id = htmlspecialchars($_GET['scripture_id']);
 echo $scripture_id;
 require "dbConnect.php";
 $db = get_db();
-
+$stmt = $db->prepare('SELECT book, chapter, verse, content FROM scriptureBD WHERE id = :id');
+$stmt->bindValue(':id', $scripture_id, PDO::PARAM_INT);
+$stmt->execute();
+$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+VAR_DUMP($scriptures);
+?>
 ?>
 
 <!DOCTYPE html>
