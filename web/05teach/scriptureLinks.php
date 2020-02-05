f171064f72a1350c30325edfca0a7ef9a2ccf3f3
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-05 13:48:14 -0500

    m

--- web/05teach/scriptureLinks.php
+++ web/05teach/scriptureLinks.php
@@ -1,16 +1,6 @@
 <?php
 $scripture_id = htmlspecialchars($_GET['scripture_id']);
-require "dbConnect.php";
-$db = get_db();
-SELECT CURRENT_TIMESTAMP;
-echo CURRENT_TIMESTAMP;
-//SELECT book, chapter, verse, content FROM scriptureBD by id;
- 
-$stmt = $db->prepare('SELECT book, chapter, verse, content FROM scriptureBD WHERE id = :id');
-$stmt->bindValue(':id', $scripture_id, PDO::PARAM_INT);
-$stmt->execute();
-$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
-VAR_DUMP($scriptures);
+echo $scripture_id;
 ?>
 
 <!DOCTYPE html>
@@ -20,17 +10,5 @@ VAR_DUMP($scriptures);
 	</head>
 <body>
 
-	<?php
-			// Go through each result
-			foreach ($scriptures as $scripture)
-			{
-				$book 			= $scripture['book'];
-				$chapter 		= $scripture['chapter'];
-				$verse 			= $scripture['verse'];
-				$content 		= $scripture['content'];
-			}
-			?>
-	<h1>Scripture Content for:<?php echo <strong>$book $chapter:$verse</strong>?></h1>
-	<p><?php echo $content?></p>
 </body>
 </html>
