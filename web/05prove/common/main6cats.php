e9365119ee7d68a06f11f5725a6f0e37cece3c06
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-06 17:39:28 -0500

    m

--- web/05prove/common/main6cats.php
+++ web/05prove/common/main6cats.php
@@ -7,6 +7,55 @@ $stmt = $db->prepare('SELECT a.artid, a.brief, a. title, a.price, a.thumb, a.ful
 $stmt->bindValue(':title', $cat, PDO::PARAM_STR);
 $stmt->execute();
 $cats = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump ($cats);
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ARFs</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/style.css" media="screen">
+        <link href="https://fonts.googleapis.com/css?family=Boogaloo|Dosis" rel="stylesheet">
+    </head>
+<main>
+    <div>
+        <!-- FIRST DIV--COMMENT:  main title     -->
+        <div class="main1" >
+            <?php include 'modules/titlecats.php'; ?>
+        </div>
+        
+        <!-- SECOND DIV--COMMENT:  CAT ARTWORK     -->
+        <div class="main2" >  
+        <ul>
+            <?php
+            var_dump ($cats);
+                // Go through each result
+                
+                foreach ($cats as $cat)
+                    {
+                    echo "<div>";
+                    $artid      = $cat['artid'];
+                    $artist     = $cat['displayname'];
+                    $brief      = $cat['brief'];
+                    $price      = $cat['price'];
+                    $thumb      = $cat['thumb'];
+                    $fullsize   = $cat['fullsize'];
+                    echo $artid;
+                    echo $artist;
+                    echo $brief;
+                    echo $price;
+                    echo $thumb;
+                    echo $fullsize;
+
+                    // echo "<li><h3><a href='biopage.php?id=$id'>$name</a><h3></li>";
+                    echo "</div>";
+                    }                
+			?>
+		</ul> 
+        <br><br> <br>                  
+        </div>
+      
+    </div>
+</main>
+</html>
 
