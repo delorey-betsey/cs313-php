b083f089f8ebcc903d3c51067c22750ac95d72ca
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-06 17:31:36 -0500

    m

--- web/05prove/common/main6cats.php
+++ web/05prove/common/main6cats.php
@@ -2,9 +2,7 @@
 echo 'entering main6cats';
 require "dbConnect.php";
 $db = get_db();
-$stmt = $db->prepare('SELECT a.artid, a.brief, a.price, a.thumb, a.fullsize u.displayname 
-FROM art a JOIN userarfs u ON a.artist = u.userid 
-WHERE a.title = :title');
+$stmt = $db->prepare('select a.artid, a.brief, a. title, a.price, a.thumb, a.fullsize, u.displayname from art a join userarfs u on a.artist = u.userid where a.title = :title'); 
 $stmt->bindValue(':title', 'cat', PDO::PARAM_str);
 $stmt->execute();
 $cats = $stmt->fetchAll(PDO::FETCH_ASSOC);
