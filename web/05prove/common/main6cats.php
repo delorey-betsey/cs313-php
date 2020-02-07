2328b6cead46fe8a4f6f4eaaac0ab94739786ec0
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-06 20:12:10 -0500

    m

--- web/05prove/common/main6cats.php
+++ web/05prove/common/main6cats.php
@@ -5,11 +5,11 @@ $db = get_db();
 $cat = 'cat';
 echo ($cat);
 
-// $stmt = $db->prepare('SELECT a.artid, a.brief, a.title, a.price, a.thumb, a.fullsize, u.displayname FROM art a JOIN userarfs u ON a.artist = u.userid WHERE a.title = :title'); 
-// $stmt->bindValue(':title', $cat, PDO::PARAM_STR);
-// $stmt->execute();
-// $cats = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// var_dump ($cats);
+$stmt = $db->prepare('SELECT a.artid, a.brief, a.title, a.price, a.thumb, a.fullsize, u.displayname FROM art a JOIN userarfs u ON a.artist = u.userid WHERE a.title = :title'); 
+$stmt->bindValue(':title', $cat, PDO::PARAM_STR);
+$stmt->execute();
+$cats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+var_dump ($cats);
 ?>
 <!DOCTYPE html>
 <html>
