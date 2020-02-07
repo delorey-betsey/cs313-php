3ed2268ee74d4185dc3fad7615d46a0631cffa63
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-06 20:45:08 -0500

    m

--- web/05prove/modules/inventory.php
+++ web/05prove/modules/inventory.php
@@ -31,56 +31,58 @@ $items = $_SESSION["items"];
         }
 ?>
 
- <!-- only do form if POST items is not set  -->
-<form method="post" action="">
-
-    <div class="addtocart">
-        <div></div>
-        <div >          
-            <!-- <input type="submit" >  -->
-            <button type="submit" name="addbutton" value="addbutton"><img src="images/addbutton.jpg" alt="addbutton"></button>      
-            <br><br>
+<?php
+require "dbConnect.php";
+$db = get_db();
+$stmt = $db->prepare('SELECT a.artid, a.brief, a.title, a.price, a.thumb, a.fullsize, u.displayname FROM art a JOIN userarfs u ON a.artist = u.userid'); 
+$stmt->execute();
+$inventory = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ARfS</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/style.css" media="screen">
+        <link href="https://fonts.googleapis.com/css?family=Boogaloo|Dosis" rel="stylesheet">
+    </head>
+<main>
+    <div>
+        <!-- FIRST DIV--COMMENT:  main title     -->
+        <div class="main1" >
+            <?php include 'modules/titlecats.php'; ?>
         </div>
+        
+        <!-- SECOND DIV--COMMENT:  CAT ARTWORK     -->
+        <div class="grouping" >  
+            <?php
+                foreach ($inventory as $item)
+                {
+                    echo "<div class='art'>";
+
+                        $artid      = $item['artid'];
+                        $artist     = $item['displayname'];
+                        $brief      = $item['brief'];
+                        $price      = $item['price'];
+                        $thumb      = $item['thumb'];
+                        $fullsize   = $item['fullsize'];
+
+                        echo "<div><br>Item Number: $artid  </div>";
+                        echo "<div><br>Created by:  $artist </div>";
+                        echo "<div><br>Description: $brief  </div>";
+                        echo "<div><br>Price:       $price  </div>";
+                        echo "<div><img src=$thumb alt= $brief /> </div>";
+                        echo "<br>";
+
+                    echo "</div>";
+                }               
+			?>
+            <br><br><br>                  
+        </div>      
     </div>
-
-    <div class="grouping">
-
-        <div class="art">
-            <div><a  class="item" href="../03prove/modules/dog1.php" title="dog1" >Item 1</a></div>       
-            <input class=“largerCheckbox” type="checkbox" id="item1" name="items[]" value="item1">
-            <br>
-            <div><img   src="images/dog1.jpg"  alt= "dog1"  /> </div>            
-            <label for="item1">Item 1 - $10</label>
-        </div>
-        <br><br>
-
-        <div class="art">   
-            <div><a class="item" href="../03prove/modules/dog2.php" title="dog2" >Item 2</a> </div>
-            <input class=“largerCheckbox” type="checkbox" id="item2" name="items[]" value="item2">
-            <br>
-            <div><img  src="images/dog2.jpg"  alt= "dog2"  /> </div>
-            <label for="item2">Item 2 - $20</label>
-        </div>
-        <br><br>
-
-        <div class="art">    
-            <div><a class="item"  href="../03prove/modules/cat1.php" title="cat1" >Item 3</a></div>
-            <input class=“largerCheckbox” type="checkbox" id="item3" name="items[]" value="item3">
-            <br>
-            <div><img  src="images/cat1.jpg"  alt= "cat1"  /> </div>
-            <label for="item3">Item 3 - $30</label>
-        </div>       
-        <br><br>
-
-        <div class="art">    
-            <div><a class="item"  href="../03prove/modules/cat2.php" title="cat2" >Item 4</a></div>            
-            <input class=“largerCheckbox” type="checkbox" id="item4" name="items[]" value="item4">
-            <br>
-            <div><img  src="images/cat3.jpg"  alt= "cat3"  /> </div>
-            <label for="item4">Item 4 - $40</label>
-        </div>
-    </div>    
-</form>
+</main>
+</html>
 
 
   
