93f20f38825f47b91f1a3c9057fc78c6456c59e3
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-18 11:40:06 -0500

    m

--- web/07teach/signIn.php
+++ web/07teach/signIn.php
@@ -86,8 +86,8 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
 // echo "<br>";
 
 		// // now check to see if the hashed password matches
-		// if (password_verify($password, $hashedPasswordFromDB))
-		if ($password = $hashedPasswordFromDB) {
+		if (password_verify($password, $hashedPasswordFromDB)) {
+		// if ($password = $hashedPasswordFromDB) {
 // echo "<br>";
 // echo "2 $ badLogin is:";
 // echo "<br>";
