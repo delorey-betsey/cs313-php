cffa7fd6ed5f4996639830bb823e14ddb930e942
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-18 15:17:05 -0500

    m

--- web/07prove/adminpage.php
+++ web/07prove/adminpage.php
@@ -1,46 +1,31 @@
-<?php
-/**********************************************************
-* File: home.php
-* Author: Br. Burton
-* 
-* Description: This is the home page. It checks that a user
-*  exists on the session and redirects to the login page
-*  if it does not.
-***********************************************************/
-session_start();
-
-// echo "session username is:";
-// echo "<br>";
-// echo $_SESSION['username'];
-// echo "<br>";
-
-if (isset($_SESSION['username']))
-{
-	$username = $_SESSION['username'];
-}
-else
-{
-	header("Location: signIn.php");
-	die(); // we always include a die after redirects.
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Admin Page</title>
-</head>
+    <head>
+        <title>ARFs</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/style.css" media="screen">
+        <link href="https://fonts.googleapis.com/css?family=Boogaloo|Dosis" rel="stylesheet">
+    </head>
 
 <body>
-<div>
+ 
+        
+    <!-- COMMENT:  flexbox header and nav    -->   
+    <?php include 'common/header.php'; ?>
+    <?php include 'common/nav.php'; ?>
 
-	<h1>Welcome to the Site Admin</h1>
 
-	Your username is: <?= $username ?><br /><br />
+    <!-- COMMENT:  flexbox main     -->  
+    <?php include 'common/main8admin.php'; ?>
 
-	<a href="signOut.php">Sign Out</a>
-</div>
+
+
+    <!-- COMMENT:  flexbox bottom nav and footer  -->  
+    <?php include 'common/nav.php'; ?>
+    <?php include 'common/footer.php'; ?>
+
 
 </body>
 </html>
+
