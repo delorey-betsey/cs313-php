99000a07bcd9cce334e5f95b45405677c5fc2853
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-19 15:37:34 -0500

    m

--- web/07prove/confirmationpage.php
+++ web/07prove/confirmationpage.php
@@ -20,7 +20,9 @@
 
 
     <!-- COMMENT:  flexbox main     -->  
-    <?php include 'common/main4confirm.php'; ?>
+    <div class="main2" >
+        <?php include 'common/main4confirm.php'; ?>
+    </div>
 
         <!-- COMMENT:  flexbox nav     -->  
         <?php include 'common/nav.php'; ?>
