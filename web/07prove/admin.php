c59f4cc29713621d6d2d8928870c26648f111ce4
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-19 17:54:55 -0500

    m

--- web/07prove/admin.php
+++ web/07prove/admin.php
@@ -33,6 +33,34 @@ $db = get_db();
 	<br /><br />
 	<h1>Welcome to ARfS Admin</h1>
 	<br><br>
+	<h2>ADD NEW ARTIST</h2>
+		<br>
+		<!-- <form class="formdiv" id="mainForm" action="" method="POST"> -->
+		<form class="formdiv" id="mainForm" action="addartist.php" method="POST">
+
+			<label for="userName">User name:</label>
+			<input type="text" id="userName" name="userName"></input>			
+			<br /><br />
+
+			<label for="userPassword">User password:</label>
+			<input type="text" id="userPassword" name="userPassword"></input>
+			<br /><br />
+
+			<label for="displayName">User display name:</label>
+			<input type="text" id="displayName" name="displayName"></input>
+			<br /><br />
+
+			<label for="pic">Picture file name:</label>
+			<input type="text" id="pic" name="pic"></input>
+			<br /><br />
+
+			<label for="bio">Short user bio:</label>
+			<input type="text" id="bio" name="bio"></input>
+			<br /><br />
+			<br />
+
+			<input type="submit" value="Add to Database" />
+		</form>
 
 </div>
 	<br><br>
