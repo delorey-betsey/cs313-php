bda673d340e39fe8c0e40840bccf7fefd6353b2f
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-20 14:25:46 -0500

    m

--- web/07prove/admin.php
+++ web/07prove/admin.php
@@ -36,7 +36,36 @@ $db = get_db();
 	<br /><br />
 	<h1>Welcome to ARfS Admin</h1>
 	<br><br>
-	
+	<?php 
+		echo "<h2>ADD NEW ARTIST</h2>";
+		echo "<br>";
+		// <!-- <form class="formdiv" id="mainForm" action="" method="POST"> -->
+		echo "<form class="formdiv" id="mainForm" action="addartist.php" method="POST">";
+
+		echo "<label for="userName">User name:</label>";
+		echo "<input type="text" id="userName" name="newuserName"></input>";			
+		echo "<br /><br />";
+
+		echo "<label for="userPassword">User password:</label>";
+		echo "<input type="text" id="userPassword" name="newuserPassword"></input>";
+		echo "<br /><br />";
+
+		echo "<label for="displayName">User display name:</label>";
+		echo "<input type="text" id="displayName" name="newdisplayName"></input>";
+		echo "<br /><br />";
+
+		echo "<label for="pic">Picture file name:</label>";
+		echo "<input type="text" id="pic" name="newpic"></input>";
+		echo "<br /><br />";
+
+		echo "<label for="bio">Short user bio:</label>";
+		echo "<input type="text" id="bio" name="newbio"></input>";
+		echo "<br /><br />";
+		echo "<br />";
+
+		echo "<input type="submit" value="Add to Database" />";
+		echo "</form>";
+	?>	
 </div>
 	<br><br>
 	<a href="signOut.php">Sign Out</a>
