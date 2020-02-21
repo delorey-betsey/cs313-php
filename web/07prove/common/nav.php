ef6d8f14094f3e4f473c5bddf8044550645043ca
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-21 12:07:47 -0500

    m

--- web/07prove/common/nav.php
+++ web/07prove/common/nav.php
@@ -1,31 +1,31 @@
 <nav>
     <!-- COMMENT:  top navigation links    -->
     <!-- COMMENT:  tools     -->
-    <div class="navlinks" class="navbuttonpadding">
+    <div class="navlinkstan" class="navbuttonpadding">
         <?php include 'modules/browselink.php'; ?>
     </div>
-    <div class="navlinks" class="navbuttonpadding">
+    <div class="navlinkstan" class="navbuttonpadding">
         <?php include 'modules/catslink.php'; ?>
     </div>
-    <div class="navlinks" class="navbuttonpadding">
+    <div class="navlinkstan" class="navbuttonpadding">
         <?php include 'modules/dogslink.php'; ?>
     </div>
-    <div class="navlinks" class="navbuttonpadding">
+    <div class="navlinkstan" class="navbuttonpadding">
         <?php include 'modules/artistslink.php'; ?>
     </div>
-    <div class="navlinks" class="navbuttonpadding">
+    <div class="navlinkstan" class="navbuttonpadding">
         <?php include 'modules/cartlink.php'; ?>
     </div>
-    <div class="navlinks" class="navbuttonpadding">
+    <div class="navlinkstan" class="navbuttonpadding">
         <?php include 'modules/checkoutlink.php'; ?>
     </div> 
     
-    <div class="navlinks" class="navbuttonpadding">       
+    <div class="navlinkstan" class="navbuttonpadding">       
     </div>
-    <div class="navlinks" class="navbuttonpadding">         
+    <div class="navlinkstan" class="navbuttonpadding">         
     </div>
 
-    <div class="navlinks" class="blacktext" class="navbuttonpadding">
+    <div class="navlinksblack" class="navbuttonpadding">
         <?php include 'modules/adminlink.php'; ?>
     </div> 
  
