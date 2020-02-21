bf2d9cddb0c4be08ee92ed17ce06e56008ac646c
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-21 13:42:44 -0500

    m

--- web/07prove/updatesold.php
+++ web/07prove/updatesold.php
@@ -12,4 +12,33 @@ if (isset($_POST['artID']))
 		header("Location: adminpage.php");
 		die(); 
 	}
+
+// to populate sold date to mark as sold
+
+if (isset($_POST['artID']) 
+	&& isset($_POST['soldDT']))
+{   
+	echo "IF for updating sold date";
+	$artID   = $_POST['artID'];
+	$soldDT  = $_POST['soldDT'];
+
+	require("dbConnect.php");
+	$db = get_db();
+
+	try { //update sold date
+		$query = 'UPDATE art SET soldDT = :soldDT WHERE artID = :artID';
+		$statement = $db->prepare($query);
+		$statement->bindValue(':soldDT',$soldDT);
+		$statement->bindValue(':artID',$artID);
+		$statement->execute();		
+		}
+	catch (Exception $ex)
+		{
+			echo "Error with DB. Details: $ex";
+			die();
+		}	
+	$_SESSION['updatemsg']  = '**Sold date updated successfully.';
+	header("Location: adminpage.php");
+	die();
+}
 ?>
