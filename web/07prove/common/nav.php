7e11f2a6adeb56dfed3a4a9a1c19f79397c96fd6
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-21 12:09:20 -0500

    m

--- web/07prove/common/nav.php
+++ web/07prove/common/nav.php
@@ -1,31 +1,31 @@
 <nav>
     <!-- COMMENT:  top navigation links    -->
     <!-- COMMENT:  tools     -->
-    <div class="navlinkstan" class="navbuttonpadding">
+    <div class="navlinkstan" >
         <?php include 'modules/browselink.php'; ?>
     </div>
-    <div class="navlinkstan" class="navbuttonpadding">
+    <div class="navlinkstan" >
         <?php include 'modules/catslink.php'; ?>
     </div>
-    <div class="navlinkstan" class="navbuttonpadding">
+    <div class="navlinkstan" >
         <?php include 'modules/dogslink.php'; ?>
     </div>
-    <div class="navlinkstan" class="navbuttonpadding">
+    <div class="navlinkstan">
         <?php include 'modules/artistslink.php'; ?>
     </div>
-    <div class="navlinkstan" class="navbuttonpadding">
+    <div class="navlinkstan">
         <?php include 'modules/cartlink.php'; ?>
     </div>
-    <div class="navlinkstan" class="navbuttonpadding">
+    <div class="navlinkstan">
         <?php include 'modules/checkoutlink.php'; ?>
     </div> 
     
-    <div class="navlinkstan" class="navbuttonpadding">       
+    <div class="navlinkstan">       
     </div>
-    <div class="navlinkstan" class="navbuttonpadding">         
+    <div class="navlinkstan">         
     </div>
 
-    <div class="navlinksblack" class="navbuttonpadding">
+    <div class="navlinksblack">
         <?php include 'modules/adminlink.php'; ?>
     </div> 
  
