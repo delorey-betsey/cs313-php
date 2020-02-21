58fe3aa2681c267fb0e68870a7e7eef49c4297c1
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-21 11:26:48 -0500

    m

--- web/07prove/updatesold.php
+++ web/07prove/updatesold.php
@@ -5,32 +5,47 @@ if (isset($_POST['artID'])
 	{
 	$artID   = $_POST['artID'];
 	$soldDT  = $_POST['soldDT'];
+	try { //update sold date
+		$query = 'UPDATE art SET soldDT = :soldDT WHERE artID = :artID';
+		$statement = $db->prepare($query);
+		$statement->bindValue(':soldDT',$soldDT);
+		$statement->bindValue(':artID',$artID);
+		$statement->execute();
+		
+		require("dbConnect.php");
+		$db = get_db();
+		}
+	catch (Exception $ex)
+		{
+			// Please be aware that you don't want to output the Exception message in
+			// a production environment
+			echo "Error with DB. Details: $ex";
+			die();
+		}
 	}
+
 if (isset($_POST['artID']) 
 	&& !isset($_POST['soldDT']))
 	{
-	$artID   = $_POST['artID'];
-	$soldDT  = NULL;
+	$artID   = $_POST['artID'];	 
+	try { //update sold date
+		$query = 'UPDATE art SET soldDT = :soldDT WHERE artID = :artID';
+		$statement = $db->prepare($query);
+		$statement->bindValue(':soldDT',NULL);
+		$statement->bindValue(':artID',$artID);
+		$statement->execute();
+		
+		require("dbConnect.php");
+		$db = get_db();
+		}
+	catch (Exception $ex)
+		{
+			// Please be aware that you don't want to output the Exception message in
+			// a production environment
+			echo "Error with DB. Details: $ex";
+			die();
+		}
 	}	
-
-require("dbConnect.php");
-$db = get_db();
-
-try { //update sold date
-	$query = 'UPDATE art SET soldDT = :soldDT WHERE artID = :artID';
-	$statement = $db->prepare($query);
-	$statement->bindValue(':soldDT',$soldDT);
-	$statement->bindValue(':artID',$artID);
-	$statement->execute();
-	}
-catch (Exception $ex)
-	{
-		// Please be aware that you don't want to output the Exception message in
-		// a production environment
-		echo "Error with DB. Details: $ex";
-		die();
-	}
-
 $_SESSION['updatemsg']  = '**Sold date update was successful.';
 header("Location: adminpage.php");
 
