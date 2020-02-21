3f857edc4ca4dd62d1fb4bfaebd44160ce66792c
Betsey Delorey <byuideloreyb@gmail.com>
2020-02-21 13:37:12 -0500

    m

--- web/07prove/updatesold.php
+++ web/07prove/updatesold.php
@@ -6,6 +6,7 @@ if (isset($_POST['artID']))
 {   
 	echo "checking IF for artid";
 	$artID   = $_POST['artID'];
+	echo $artID;
 	} else
 	{
 		header("Location: adminpage.php");
@@ -13,57 +14,5 @@ if (isset($_POST['artID']))
 	}
 }	
 
-// to set sold date back to NULL
 
-if (!isset($_POST['soldDT']))
-{   
-	echo "checking IF for no sold date";
-	$artID   = $_POST['artID'];
-
-	require("dbConnect.php");
-	$db = get_db();
-	try { //update sold date
-		$query = 'UPDATE art SET soldDT = NULL WHERE artID = :artID';
-		$statement = $db->prepare($query);
-		$statement->bindValue(':artID',$artID);
-		$statement->execute();		
-		}
-	catch (Exception $ex)
-		{
-			echo "Error with DB. Details: $ex";
-			die();
-		}	
-	$_SESSION['updatemsg']  = '**Sold date set to NULL.';
-	header("Location: adminpage.php");
-	die(); 
-}
-
-// to populate sold date to mark as sold
-
-if (isset($_POST['artID']) 
-	&& isset($_POST['soldDT']))
-{   
-	echo "IF for updating sold date";
-	$artID   = $_POST['artID'];
-	$soldDT  = $_POST['soldDT'];
-
-	require("dbConnect.php");
-	$db = get_db();
-
-	try { //update sold date
-		$query = 'UPDATE art SET soldDT = :soldDT WHERE artID = :artID';
-		$statement = $db->prepare($query);
-		$statement->bindValue(':soldDT',$soldDT);
-		$statement->bindValue(':artID',$artID);
-		$statement->execute();		
-		}
-	catch (Exception $ex)
-		{
-			echo "Error with DB. Details: $ex";
-			die();
-		}	
-	$_SESSION['updatemsg']  = '**Sold date updated successfully.';
-	header("Location: adminpage.php");
-	die();
-}
 ?>
